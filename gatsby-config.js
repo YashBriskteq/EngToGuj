eca26ccdbde9de4aa6523db6d6341db8d6624d2f
Yash Pandya <yash.pandya@briskteq.com>
2023-04-03 12:45:16 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `localization`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
